e50c9407fdfff57d6b0c960d89416ad42fcf0d57
xianyu.hxy <hxy0107@yeah.net>
2017-10-23 21:54:39 +0800

    添加webview
    添加webview

--- app/src/main/java/com/harry/unirecruit/MainActivity.java
+++ app/src/main/java/com/harry/unirecruit/MainActivity.java
@@ -1,15 +1,33 @@
 package com.harry.unirecruit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.harry.unirecruit.ui.BrowserActivity;
+
 public class MainActivity extends AppCompatActivity {
+    public static final int MODE_DEFAULT = 0;
+
+    public static final int MODE_SONIC = 1;
+
+    public static final int MODE_SONIC_WITH_OFFLINE_CACHE = 2;
+
+    public static final int MODE_LOAD_CACHE_ELSE_NETWORK=3;
+
+    private static final int PERMISSION_REQUEST_CODE_STORAGE = 1;
+
+    private static final String DEMO_URL = "http://mc.vip.qq.com/demo/indexv3";
 
     private TextView mTextMessage;
+    private Button mButton;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,9 +55,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mTextMessage = (TextView) findViewById(R.id.message);
+        mButton=(Button)findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startBrowserActivity(MODE_LOAD_CACHE_ELSE_NETWORK);
+
+            }
+        });
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+
+    private void startBrowserActivity(int mode) {
+        Intent intent = new Intent(this, BrowserActivity.class);
+        intent.putExtra(BrowserActivity.PARAM_URL, "http://hr.whu.edu.cn/");
+        intent.putExtra(BrowserActivity.PARAM_MODE, mode);
+        intent.putExtra(SonicJavaScriptInterface.PARAM_CLICK_TIME, System.currentTimeMillis());
+        startActivityForResult(intent, -1);
+    }
 }
---
+++ app/src/main/java/com/harry/unirecruit/ui/MainFragment.java
@@ -0,0 +1,18 @@
+package com.harry.unirecruit.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+/**
+ * Created by 毕空 on 2017/10/23.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
