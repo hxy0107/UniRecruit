d6301a62d8168243b5f4227ec38ddb8c50ff7af9
xianyu.hxy <xianyu.hxy@alibaba-inc.com>
2017-11-18 15:57:30 +0800

    第三方
    热大范甘迪食发鬼

--- app/src/main/java/com/harry/unirecruit/utils/SharedPreUtils.java
+++ app/src/main/java/com/harry/unirecruit/utils/SharedPreUtils.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.Log;
